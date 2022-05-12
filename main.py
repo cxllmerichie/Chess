033046abb7e8023792ada8a196d0711e2d8d5261
cxllmerichie <cxllmerichie@gmail.com>
2022-05-12 19:02:59 +0200

    Significant update. Multiplayer (promotion is bagging), practice (works), singleplayer (in process).

--- main.py
+++ main.py
@@ -5,26 +5,28 @@
 # ToDo: loser&winner, starting messages etc.
 # ToDo: MULTI multiplayer
 # ToDo: fix promotion
-# ToDo: fix buttons draw
+# ToDo: fix buttons draw (if we wait too long after draw was suggested the responding client crashes)
 # ToDo: self-calculating bytes amount for client and server
 
+# from ServerClient.config import IP, PORT
+# from ServerClient.ServerClient import server_startup
+# from ClientMultipalyer.Library import State
+# from _thread import start_new_thread
+# from socket import socket, AF_INET, SOCK_STREAM
+# from time import sleep
+
 from PyQt5.QtWidgets import QApplication
-from Client.Application import Application
-from Server.config import IP, PORT
-from Server.Server import server_startup
-from Client.Library import State
-from _thread import start_new_thread
-from socket import socket, AF_INET, SOCK_STREAM
+from Common.Application import Application
 from sys import argv
 
 
 if __name__ == '__main__':
-    checkchange
-    server: socket = socket(AF_INET, SOCK_STREAM)
-    if server.connect_ex((IP, PORT)) is not State.Started.value:
-        start_new_thread(server_startup, ())
-    server.close()
+    # server: socket = socket(AF_INET, SOCK_STREAM)
+    # if server.connect_ex((IP, PORT)) is not State.Started.value:
+    #     start_new_thread(server_startup, ())
+    # server.close()
+    # sleep(600)
 
     application: QApplication = QApplication(argv)
-    Application()
+    app: Application = Application()
     application.exec()
